e66eb3101f3c9f5bba498b6e5bbc95a06c01c11d
Naveed Massjouni <naveedm9@gmail.com>
2012-04-07 05:30:13 +0000

    Make month name dynamic

--- lib/RackerTracker.pm
+++ lib/RackerTracker.pm
@@ -1,15 +1,12 @@
 package RackerTracker;
 use Dancer ':syntax';
-
 use Dancer::Plugin::DBIC 'schema';
-use Data::Dump 'dump';
 use DateTime;
 
-get '/' => sub {
-    template 'officecomp15';
-};
+get '/' => sub { template 'officecomp15' };
 
-#{'email' => '','exercise' => ['1332691921129']}
+get '/ajax/workouts' => sub { _get_data() };
+
 post '/ajax/workouts' => sub {
     my $data = from_json request->body;
     my $email = $data->{email};
@@ -19,31 +16,31 @@ post '/ajax/workouts' => sub {
     }
     $email = lc $email;
     my $user = schema->resultset('User')->find_or_create({ email => $email });
-    my $current_name = $user->email;
-    $current_name =~ s/\@mailtrust\.com$//;
-
-    for my $epoch ( @{ $data->{exercise} } ) {
+    foreach my $epoch ( @{ $data->{exercise} } ) {
         $epoch = substr $epoch, 0, 10;
         my $date = DateTime->from_epoch(epoch => $epoch);
         $user->workouts->update_or_create({day => $date->ymd});
     }
-    return _get_data($current_name);
+    return _get_data();
 };
 
-get '/ajax/workouts' => sub { _get_data() };
-
 sub _get_data {
-    my ($current_name) = @_;
     my @users = schema->resultset('Workout')->search({}, {
         '+select' => [{ count => 'email' }],
         '+as'     => ['cnt'],
         group_by  => 'email',
     });
-    @users = map {
-        email => $_->get_column('email'),
-        score => $_->get_column('cnt'),
-    }, @users;
-    return { users => \@users };
+    @users =
+        map {
+            email => $_->get_column('email'),
+            score => $_->get_column('cnt'),
+        },
+        sort { $b->get_column('cnt') <=> $a->get_column('cnt') }
+        @users;
+    return {
+        users => \@users,
+        month => DateTime->now->month_name,
+    };
 }
 
 true;
